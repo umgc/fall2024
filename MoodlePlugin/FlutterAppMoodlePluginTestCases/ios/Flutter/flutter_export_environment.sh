d8967d89995a96195eedc81004e4b02d38099b70
Conor Moore <conor4508@gmail.com>
2024-10-03 21:05:22 -0400

    Removed example dir

--- MoodlePlugin/FlutterAppMoodlePluginTestCases/ios/Flutter/flutter_export_environment.sh
+++ MoodlePlugin/FlutterAppMoodlePluginTestCases/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=J:\Users\Conor Moore\Downloads\UMGC\flutter_windows_3.24.3-stable\flutter"
-export "FLUTTER_APPLICATION_PATH=J:\Users\Conor Moore\Downloads\UMGC\fall2024\Example"
+export "FLUTTER_APPLICATION_PATH=J:\Users\Conor Moore\Downloads\UMGC\fall2024\MoodlePlugin\FlutterAppMoodlePluginTestCases"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.1"
-export "FLUTTER_BUILD_NUMBER=1"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
