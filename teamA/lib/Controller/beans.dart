af2f04f341f075d11be39778f43f663cfc2a4929
DavidWorthy <david.s.worthington.2000@gmail.com>
2024-10-14 14:49:54 +0200

    Commented out unused variant of Rubric

--- teamA/lib/Controller/beans.dart
+++ teamA/lib/Controller/beans.dart
@@ -43,100 +43,101 @@ class XmlConsts
 }
 
 // A generated rubric containing criteria for an essay prompt.
-class Rubric 
-{
-  String title;
-  String subject;
-  String gradeLevel;
-  int maxPoints;
-  List<RubricCriteria> criteriaList;
+// Commented out as the only other instance of Rubric being used is the other one.
+// class Rubric 
+// {
+//   String title;
+//   String subject;
+//   String gradeLevel;
+//   int maxPoints;
+//   List<RubricCriteria> criteriaList;
 
-  Rubric({
-    required this.title,
-    required this.subject,
-    required this.gradeLevel,
-    required this.maxPoints,
-    required this.criteriaList,
-  });
+//   Rubric({
+//     required this.title,
+//     required this.subject,
+//     required this.gradeLevel,
+//     required this.maxPoints,
+//     required this.criteriaList,
+//   });
 
-  // Factory constructor to create a Rubric from XML
-  factory Rubric.fromXmlString(String xmlStr) 
-  {
-    final document = XmlDocument.parse(xmlStr);
-    final rubricElement = document.getElement(XmlConsts.rubric);
+//   // Factory constructor to create a Rubric from XML
+//   factory Rubric.fromXmlString(String xmlStr) 
+//   {
+//     final document = XmlDocument.parse(xmlStr);
+//     final rubricElement = document.getElement(XmlConsts.rubric);
 
-    return Rubric(
-      title: rubricElement?.getElement(XmlConsts.title)?.innerText ?? 'Untitled',
-      subject: rubricElement?.getElement(XmlConsts.subject)?.innerText ?? 'Unknown',
-      gradeLevel: rubricElement?.getElement(XmlConsts.gradeLevel)?.innerText ?? 'Unknown',
-      maxPoints: int.parse(rubricElement?.getElement(XmlConsts.maxPoints)?.innerText ?? '0'),
-      criteriaList: rubricElement
-          ?.findElements(XmlConsts.criteria)
-          .map((e) => RubricCriteria.fromXml(e))
-          .toList() ?? [],
-    );
-  }
+//     return Rubric(
+//       title: rubricElement?.getElement(XmlConsts.title)?.innerText ?? 'Untitled',
+//       subject: rubricElement?.getElement(XmlConsts.subject)?.innerText ?? 'Unknown',
+//       gradeLevel: rubricElement?.getElement(XmlConsts.gradeLevel)?.innerText ?? 'Unknown',
+//       maxPoints: int.parse(rubricElement?.getElement(XmlConsts.maxPoints)?.innerText ?? '0'),
+//       criteriaList: rubricElement
+//           ?.findElements(XmlConsts.criteria)
+//           .map((e) => RubricCriteria.fromXml(e))
+//           .toList() ?? [],
+//     );
+//   }
 
-    // Convert the Rubric object to an XML string
-  String toXmlString() {
-    final builder = XmlBuilder();
-    builder.element(XmlConsts.rubric, nest: () {
-      builder.element(XmlConsts.title, nest: title);
-      builder.element(XmlConsts.subject, nest: subject);
-      builder.element(XmlConsts.gradeLevel, nest: gradeLevel);
-      builder.element(XmlConsts.maxPoints, nest: maxPoints.toString());
+//     // Convert the Rubric object to an XML string
+//   String toXmlString() {
+//     final builder = XmlBuilder();
+//     builder.element(XmlConsts.rubric, nest: () {
+//       builder.element(XmlConsts.title, nest: title);
+//       builder.element(XmlConsts.subject, nest: subject);
+//       builder.element(XmlConsts.gradeLevel, nest: gradeLevel);
+//       builder.element(XmlConsts.maxPoints, nest: maxPoints.toString());
 
-      for (var criteria in criteriaList) {
-        builder.element(XmlConsts.criteria, nest: criteria.toXml);
-      }
-    });
-    return builder.buildDocument().toXmlString(pretty: true);
-  }
+//       for (var criteria in criteriaList) {
+//         builder.element(XmlConsts.criteria, nest: criteria.toXml);
+//       }
+//     });
+//     return builder.buildDocument().toXmlString(pretty: true);
+//   }
 
-  @override
-  String toString() {
-    return toXmlString();
-  }
-}
+//   @override
+//   String toString() {
+//     return toXmlString();
+//   }
+// }
 
-// Specific Rubric Criteria
-class RubricCriteria 
-{
-  String description;
-  int points;
-  String feedback;
+// // Specific Rubric Criteria
+// class RubricCriteria 
+// {
+//   String description;
+//   int points;
+//   String feedback;
 
-  RubricCriteria({
-    required this.description,
-    required this.points,
-    this.feedback = '',
-  });
+//   RubricCriteria({
+//     required this.description,
+//     required this.points,
+//     this.feedback = '',
+//   });
 
-  // Factory constructor to create criteria from XML
-  factory RubricCriteria.fromXml(XmlElement criteriaElement) 
-  {
-    return RubricCriteria(
-      description: criteriaElement.getElement(XmlConsts.description)?.innerText ?? 'Unknown',
-      points: int.parse(criteriaElement.getElement(XmlConsts.points)?.innerText ?? '0'),
-      feedback: criteriaElement.getElement(XmlConsts.feedback)?.innerText ?? '',
-    );
-  }
+//   // Factory constructor to create criteria from XML
+//   factory RubricCriteria.fromXml(XmlElement criteriaElement) 
+//   {
+//     return RubricCriteria(
+//       description: criteriaElement.getElement(XmlConsts.description)?.innerText ?? 'Unknown',
+//       points: int.parse(criteriaElement.getElement(XmlConsts.points)?.innerText ?? '0'),
+//       feedback: criteriaElement.getElement(XmlConsts.feedback)?.innerText ?? '',
+//     );
+//   }
 
-  // Convert the criteria to XML format
-  void toXml(XmlBuilder builder) 
-  {
-    builder.element(XmlConsts.description, nest: description);
-    builder.element(XmlConsts.points, nest: points.toString());
-    builder.element(XmlConsts.feedback, nest: feedback);
-  }
+//   // Convert the criteria to XML format
+//   void toXml(XmlBuilder builder) 
+//   {
+//     builder.element(XmlConsts.description, nest: description);
+//     builder.element(XmlConsts.points, nest: points.toString());
+//     builder.element(XmlConsts.feedback, nest: feedback);
+//   }
 
-  @override
-  String toString() {
-    final builder = XmlBuilder();
-    toXml(builder);
-    return builder.buildFragment().toXmlString();
-  }
-}
+//   @override
+//   String toString() {
+//     final builder = XmlBuilder();
+//     toXml(builder);
+//     return builder.buildFragment().toXmlString();
+//   }
+// }
 
 class Essay {
   //todo more vars as needed (like the Rubric for starters)
