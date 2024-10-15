c622cd952f367415014f6d195d4e778ca4de4348
camilledj27 <74025227+camilledj27@users.noreply.github.com>
2024-10-15 17:48:05 -0400

    Claud API Module

---
+++ teamB/evalu_ai/lib/api/llm/claude_api.dart
@@ -0,0 +1,103 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ClaudeAiAPI {
+  final String claudeAiKey;
+  ClaudeAiAPI(this.claudeAiKey);
+
+  Map<String, dynamic> convertHttpRespToJson(String httpResponseString) {
+    return (json.decode(httpResponseString) as Map<String, dynamic>);
+  }
+
+  String getPostBody(String queryMessage) {
+    return jsonEncode({
+      'model': 'claude-3-5-sonnet-20240620',
+      'max_tokens': 1024,
+      'system': 'Be precise and concise',
+      'messages': [
+        {'role': 'user', 'content': queryMessage}
+      ]
+    });
+  }
+
+  Map<String, String> getPostHeaders() {
+    return ({
+      'anthropic-version': '2023-06-01',
+      'content-type': 'application/json',
+      'x-api-key': claudeAiKey,
+    });
+  }
+
+  Uri getPostUrl() => Uri.https('api.anthropic.com', '/v1/messages');
+
+  Future<String> postMessage(
+      Uri url, Map<String, String> postHeaders, Object postBody) async {
+    final httpPackageResponse =
+        await http.post(url, headers: postHeaders, body: postBody);
+
+    if (httpPackageResponse.statusCode != 200) {
+      print('Failed to retrieve the http package!');
+      print('statusCode :  ${httpPackageResponse.statusCode}');
+      return "";
+    }
+
+    return httpPackageResponse.body;
+  }
+
+  List<String> parseQueryResponse(String resp) {
+    // ignore: prefer_adjacent_string_concatenation
+    String quizRegExp =
+        // r'(<\?xml.*?\?>\s*<quiz>(\s*.*?<question>\s*.*?<text>\s*(.*?)</text>\s*.*?<options>(\s*.*?<option>\s*(.*?)</option>)+\s*</options>\s*.*?<answer>\s*(.*?)</answer>\s*.*?</question>)+\s*</quiz>)';
+        r'(<\?xml.*?\?>\s*<quiz>.*?</quiz>)';
+
+    RegExp exp = RegExp(quizRegExp);
+    String respNoNewlines = resp.replaceAll('\n', '');
+    Iterable<RegExpMatch> matches = exp.allMatches(respNoNewlines);
+    List<String> parsedResp = [];
+
+    print("Parsing the query response - matches: $matches");
+
+    for (final m in matches) {
+      if (m.group(0) != null) {
+        parsedResp.add(m.group(0)!);
+
+        print("This is a match : ${m.group(0)}");
+        print("Number of groups in the match: ${m.groupCount}");
+        print("parsedResp : $parsedResp");
+      }
+    }
+
+    return parsedResp;
+  }
+
+  Future<String> postToLlm(String queryPrompt) async {
+    var resp = "";
+
+    // use the following test query so Perplexity doesn't charge
+    // 'How many stars are there in our galaxy?'
+    if (queryPrompt.isNotEmpty) {
+      resp = await queryAI(queryPrompt);
+    }
+    return resp;
+  }
+
+  Future<String> queryAI(String query) async {
+    final postHeaders = getPostHeaders();
+    final postBody = getPostBody(query);
+    final httpPackageUrl = getPostUrl();
+
+    final httpPackageRespString =
+        await postMessage(httpPackageUrl, postHeaders, postBody);
+
+    final httpPackageResponseJson =
+        convertHttpRespToJson(httpPackageRespString);
+
+    var retResponse = "";
+    for (var respChoice in httpPackageResponseJson['content']) {
+      retResponse += respChoice['text'];
+    }
+    // print("In queryAI - content :  $retResponse");
+    return retResponse;
+  }
+}
