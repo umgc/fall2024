a038574d5680a5e8241cc9eec16f81e733031372
Desmond H. <dmh10792@gmail.com>
2024-10-17 22:03:38 -0500

    Added navbar to almost all screens

--- teamB/evalu_ai/macos/Flutter/GeneratedPluginRegistrant.swift
+++ teamB/evalu_ai/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import device_info_plus
 import file_selector_macos
 import path_provider_foundation
 import quill_native_bridge
@@ -13,7 +12,6 @@ import shared_preferences_foundation
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   QuillNativeBridgePlugin.register(with: registry.registrar(forPlugin: "QuillNativeBridgePlugin"))
